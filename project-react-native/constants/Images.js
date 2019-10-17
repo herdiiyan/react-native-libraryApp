cf22ce186e0af6ba84b5f3a210362264929247e0
herdiiyan <herdiiyan8@gmail.com>
2019-10-17 10:54:44 +0700

    Add picture and contansts

---
+++ project-react-native/constants/Images.js
@@ -0,0 +1,8 @@
+// local imgs
+const Onboarding = require("../assets/imgs/bg.png");
+const LogoOnboarding = require("../assets/imgs/story.png");
+
+export default {
+  Onboarding,
+  LogoOnboarding,
+};
